e454ba5ad996442376dab8d709f9dd7a577ae457
Mackenzie Maxwell <mackenzie.max.rj@gmail.com>
2022-10-23 18:54:37 -0300

    First class: position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,13 @@
 package application;
 
+import boardgame.Position;
+import com.sun.source.tree.NewArrayTree;
+
 public class Program {
     public static void main(String[] args) {
+
+        Position position = new Position(3, 6);
+        System.out.println(position);
 
     }
 }
